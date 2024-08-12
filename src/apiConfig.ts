9efb261e0b6979f701ec0f43674e7d9a66628a22
ZEROONZE <luke_adm@hotmail.com>
2024-08-12 08:22:23 -0300

    colocar chave no env

--- src/apiConfig.ts
+++
@@ -1,4 +0,0 @@
-export const API_KEY = process.env.REACT_APP_API_KEY as string;
-export const PLAYLIST_ID = process.env.REACT_APP_PLAYLIST_ID as string;
-
-console.log("teste", API_KEY, PLAYLIST_ID);
